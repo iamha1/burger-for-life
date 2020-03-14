c12ac76bfccc60340d64c73a526e1224ec198642
Ha Nguyen <nguyenthingocha2@gmail.com>
2020-03-13 23:27:54 -0400

    initial commit and set-up

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Le Big Mac', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Big Kahuna Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Royale With Cheese', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Animal Style Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Shroom Burger', FALSE);
+
